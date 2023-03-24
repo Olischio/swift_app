c2d3a87c22874e4041a79c719b7f372e8e96297d
olischio <oliver.schiott@outlook.com>
2023-03-25 00:10:04 +0100

    Initial Commit

---
+++ apiapp/apiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiappApp.swift
+//  apiapp
+//
+//  Created by Oliver Schiøtt on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct apiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
